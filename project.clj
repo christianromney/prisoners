35ff1c4c29602c5dc6f6e1a6d903ec10db58068c
Christian Romney <cromney@pointslope.com>
2011-08-12 22:29:42 -0400

    Adding marginalia for documentation

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject prisoner "0.0.1-SNAPSHOT"
   :description "A Prisoner's Dilemma simulation"
   :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[lein-marginalia "0.6.0"]]
   :aot [prisoner.core])
