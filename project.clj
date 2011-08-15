f915a2edccc6698beef5b4f55d6594cd0527f779
Christian Romney <cromney@pointslope.com>
2011-08-14 21:41:46 -0400

    Added clojure.contrib dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject prisoners "0.0.2-SNAPSHOT"
   :description "A Prisoner's Dilemma simulation"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[lein-marginalia "0.6.0"]]
+  :dependencies [[org.clojure/clojure "1.2.1"] 
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :aot [prisoners.core])
