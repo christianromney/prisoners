74e1462d2b7b4484c5eb0d5e7b35ab32c530caa1
Christian Romney <cromney@pointslope.com>
2011-11-14 03:43:22 -0500

    Version bump for new API / dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prisoners "0.1.1"
+(defproject prisoners "0.2.0"
   :description "A Prisoner's Dilemma simulation"
   :dev-dependencies [[lein-marginalia "0.6.1"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
