e2565a296e083a100a84f88114738ea281627897
Christian Romney <cromney@pointslope.com>
2011-11-14 03:46:39 -0500

    Point release to fix documentation

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prisoners "0.2.0"
+(defproject prisoners "0.2.1"
   :description "A Prisoner's Dilemma simulation"
   :dev-dependencies [[lein-marginalia "0.6.1"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
