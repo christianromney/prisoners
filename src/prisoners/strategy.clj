b17e9bf6a47490234c8001ccb836e9317a2983c8
Christian Romney <cromney@pointslope.com>
2014-02-28 18:20:55 -0500

    Abstracted move selection into Playable protocol

--- src/prisoners/strategy.clj
+++ src/prisoners/strategy.clj
@@ -1,16 +1,31 @@
 (ns prisoners.strategy
   (:require [prisoners.utils :as utils]))
 
+(defprotocol Playable
+  "A playable abstraction has an internal play function
+   that implements the mechanics of making a move"
+  (-play [playable move]))
+
+
 (defprotocol Scorable
-  (score [strategy])
+  "A scorable abstraction reports its scores and total"
+  (scores [strategy])
+
   (total [strategy]))
 
 (defrecord Strategy [name points plays opponent]
   Scorable
-  (score [strategy]
-    (->> strategy :points (reductions +)))
-  (total [strategy]
-    (reduce + (:points strategy))))
+
+  (scores [this]
+    (->> this :points (reductions +)))
+
+  (total [this]
+    (reduce + (:points this)))
+
+  Playable
+
+  (-play [this move]
+    (update-in this [:plays] conj move)))
 
 (defn strategy-constructor
   "Initializes a data structure for the named strategy."
@@ -25,10 +40,10 @@
 
 ;; The `:sucker` strategy always cooperates
 (defmethod play :sucker [this]
-  (utils/add-to this :plays :coop))
+  (-play this :coop))
 
 ;; The `:random` strategy is a baseline for comparison.
 ;; Any given move is randomly chosen to be cooperate or
 ;; defect.
 (defmethod play :random [this]
-  (utils/add-to this :plays (rand-nth [:defect :coop])))
+  (-play this (rand-nth [:defect :coop])))
